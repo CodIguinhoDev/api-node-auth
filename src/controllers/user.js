4f7f9be8099960c7a77ac306207072ff56f1266b
Igor <igor.lsilva3110@gmail.com>
2026-07-01 13:56:58 -0300

    feat: adicionando os controladores

---
+++ src/controllers/user.js
@@ -0,0 +1,29 @@
+import { entry, registration } from "../services/auth.js";
+import jwt from "jsonwebtoken";
+import User from "../models/modelUser.js";
+
+export const register = async (req, res) => {
+  try {
+    const user = await registration(req.body);
+    return res.status(201).json(user);
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const user = await entry(req.body);
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
+export const message = async (_, res) => {
+  try {
+    return res.status(202).json({ message: "Login efetuado!" });
+  } catch (error) {
+    return res.status(401).json({ message: error.message });
+  }
+};
